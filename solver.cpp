cf061445efb4a464d7445538fe2e53cb7541b2af
javsav <javed.alam@student.adelaide.edu.au>
2024-09-19 01:49:24 +0930

    improved efficiency

--- solver.cpp
+++ solver.cpp
@@ -2,60 +2,42 @@
 #include <iostream>
 #include <vector>
 
-bool checkBoard(std::vector<std::vector<char>>& board,  bool (&rows)[9][9], bool (&cols)[9][9], bool (&squares)[9][9]) {
+void checkBoard(std::vector<std::vector<char>>& board,  bool (&rows)[9][9], bool (&cols)[9][9], bool (&squares)[9][9]) {
   for (int r = 0; r < 9; r++) {
     for (int c = 0; c < 9; c++) {
       if (board[r][c] != '.') {
         int current = board[r][c] - 49;
-        int sqindex = (c / 3 + ((r / 3) * 3));
-        if (rows[r][current] || cols[c][current] || squares[sqindex][current]) {
-          return false;
-        } else {
-          rows[r][current] = cols[c][current] = squares[sqindex][current] = 1;
+        int sqindex = (c / 3 + ((r / 3) * 3));        
+        rows[r][current] = cols[c][current] = squares[sqindex][current] = 1;
         }
       }
-    }
+    }  
+  return;
+}
+
+bool isValidEntry(std::vector<std::vector<char>>& board, int c, int row, int column, bool (&rows)[9][9], bool (&cols)[9][9], bool (&squares)[9][9]) {
+  int sqIndex = (column / 3 + ((row / 3) * 3));
+  if (rows[row][c] || cols[column][c] || squares[sqIndex][c]) {
+    return false;
+  } else {
+    rows[row][c] = cols[column][c] = squares[sqIndex][c] = 1;
   }
   return true;
 }
 
-bool isValidEntry(std::vector<std::vector<char>>& board, char c, int row, int column, bool (&rows)[9][9], bool (&cols)[9][9], bool (&squares)[9][9]) {
-  int current = c - 49;
-  int sqindex = (column / 3 + ((row / 3) * 3));
-  if (rows[row][current] || cols[column][current] || squares[sqindex][current]) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-void updateBoard(std::vector<std::vector<char>>& board, int row, int column, bool (&rows)[9][9], bool (&cols)[9][9], bool (&squares)[9][9]) {
-  int current = board[row][column] - 49;
-  int sqindex = (column / 3 + ((row / 3) * 3));
-
-  rows[row][current] = cols[column][current] = squares[sqindex][current] = 1;
-}
-
-bool solveSudokuHelper(std::vector<std::vector<char>>& board) {
-  bool rows[9][9] = {0};
-  bool cols[9][9] = {0};
-  bool squares[9][9] = {0};
-  checkBoard(board, rows, cols, squares);
-
+bool solveSudokuHelper(std::vector<std::vector<char>>& board, bool (&rows)[9][9], bool (&cols)[9][9], bool (&squares)[9][9]) { 
   for (int m = 0; m < 9; m++) {
     for (int n = 0; n < 9; n++) {
       if (board[m][n] == '.') {
-        for (char c = '1'; c <= '9'; c++) {
+        for (int c = 0; c < 9; c++) {
           if (isValidEntry(board, c, m, n, rows, cols, squares)) {
-            board[m][n] = c;
-            updateBoard(board, m, n, rows, cols, squares);            
-            if (solveSudokuHelper(board)) {
+            board[m][n] = c + 49;
+            if (solveSudokuHelper(board, rows, cols, squares)) {
               return true;
             } else {
-              int cIndex = c - 49;
-              rows[m][cIndex] = false;
-              cols[m][cIndex] = false;
-              squares[(n / 3 + ((m / 3) * 3))][cIndex] = false;
+              rows[m][c] = false;
+              cols[n][c] = false;
+              squares[(n / 3 + ((m / 3) * 3))][c] = false;
               board[m][n] = '.';
             }
           }
@@ -68,7 +50,11 @@ return true;
 }
 
 void solveSudoku(std::vector<std::vector<char>>& board) {
-  solveSudokuHelper(board);
+  bool rows[9][9] = {0};
+  bool cols[9][9] = {0};
+  bool squares[9][9] = {0};
+  checkBoard(board, rows, cols, squares);
+  solveSudokuHelper(board, rows, cols, squares);
 }
 
   int main() {
@@ -93,6 +79,12 @@ void solveSudoku(std::vector<std::vector<char>>& board) {
   auto t1 = high_resolution_clock::now();
   solveSudoku(sudoku);  
   auto t2 = high_resolution_clock::now();
+  for (auto vec:sudoku) {
+    for (auto c:vec) {
+      std::cout << c << ' ';
+    }
+    std::cout << '\n';
+  }
   /* Getting number of milliseconds as an integer. */
   auto ms_int = duration_cast<milliseconds>(t2 - t1);
   std::cout << ms_int.count() << "ms\n";
